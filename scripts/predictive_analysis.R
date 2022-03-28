6edfae05ab61cb949ff8f440ab37addc1d92b9f5
David Nguyen <david.th.nguy@gmail.com>
2022-03-28 17:20:00 -0500

    preliminary OTU selection using elastic net

--- scripts/predictive_analysis.R
+++ scripts/predictive_analysis.R
@@ -47,8 +47,111 @@ nrow(asv)
 (n_OTU * (nrow(life_hist) - 2) ) == nrow(asv)
 
 ### Fit models
-# only use subset of lifehistory that intersects with samples in 16s
+# only use intersection of samples between life hist and 16s data sets
 life_hist_subset <- 
   life_hist %>%
-  filter(Sample %in% asv_samples)
+  filter(Sample %in% asv_samples) %>%
+  filter(!is.na(avg_final_size), !is.na(avg_cumulative_reproduction))
 
+asv_wide <- asv_wide %>% filter(Sample %in% life_hist_subset$Sample)
+
+# number of samples per treatment groups is ... weird bc of split-brood design
+# could make problem with cross-validation if we use these as predictors
+# maybe could use generations 1:5 from 15 gen expt as out of sample test?
+life_hist_subset %>%
+  group_by(generation, trt_type) %>% 
+  summarise(n())
+
+life_hist_subset %>%
+  group_by(treatment) %>% 
+  summarise(n())
+
+# save each df arranged in alpha order by sample
+# necessary because function for elastic net / lasso require vector reponse and matrix X (no data argument)
+life_hist_subset <- life_hist_subset %>% arrange(Sample)
+asv_wide <- asv_wide %>% arrange(Sample)
+
+# check order of samples
+all.equal(asv_wide$Sample, life_hist_subset$Sample)
+
+# load package for elastic net regression
+library(glmnet)
+
+# make sparse design matrix
+library(Matrix)
+asv_mat <- as(as.matrix(asv_wide[,-1]), "sparseMatrix")
+
+# 40 % of size of original matrix
+# will speed up model fititing
+# need to learn how to make sparse model matrix to accomodate interactions in the future
+object.size(asv_mat) / object.size(asv_wide[,-1])
+
+# set number of grid points for alpha
+n_alpha <- 10
+
+# init list to store cv objects
+fit_list <- vector("list", length = length(0:10))
+
+# set foldid
+set.seed(123)
+foldid <- sample(rep(1:10,length.out = nrow(life_hist_subset))) # control which obs go to which folds in CV
+
+# fit for all values of alpha ranging from ridge to lasso
+for (iter in 0:10) {
+fit_list[[iter+1]] <- cv.glmnet(y = life_hist_subset$avg_final_size,
+                                   x = asv_mat,
+                                   type.measure = "mse",
+                              foldid = foldid,
+                                   alpha = (iter / 10))
+print(iter)
+}
+
+
+# get the minimum mean c
+all_cv <- data.frame(alpha = 0:10 / 10
+           , min_cv_mean = sapply(seq_along(fit_list), function(fit) min(fit_list[[fit]]$cvm))
+           #, cv_sd = sapply(seq_along(fit_list), function(fit) min(fit_list[[fit]]$cvsd))
+           )
+
+plot(min_cv_mean ~ alpha, data = all_cv)
+
+best_mod <- fit_list[[2]]
+best_mod
+
+best_coef <- coef(best_mod, s = "lambda.1se")[-1] # drop intercept
+
+best_coef = as.data.frame(as.matrix(best_coef))
+indices = which(best_coef != 0)
+key_otu = colnames(asv_mat)[indices]
+length(key_otu)
+
+coef_df <- data.frame(OTU = key_otu,
+                      beta = best_coef[indices,])
+
+asv <- asv %>% mutate(selected = OTU %in% key_otu)
+
+asv_selected <- left_join(asv, coef_df)
+
+# plot stuff
+asv_selected %>%
+  ggplot(aes(group = Order, y = beta)) +
+  geom_boxplot()
+
+# 
+coef_df %>%
+  arrange(beta) %>% hist
+
+hist(coef_df$beta)
+
+# find largest magnitude
+asv_selected %>%
+  filter(!is.na(beta)) %>%
+  arrange(beta) %>%
+  slice_head(n = 10) %>%
+  select(Kingdom:beta)
+
+asv_selected %>%
+  filter(!is.na(beta)) %>%
+  arrange(beta) %>%
+  slice_tail(n = 10) %>%
+  select(Kingdom:beta)
